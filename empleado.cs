717b0d7aeb0c2c18e56458e87025faa31f33cb26
0ignacio <ignaciogonzaley@gmail.com>
2023-06-01 19:51:13 -0300

    punto2 v3

--- empleado.cs
+++ empleado.cs
@@ -1,24 +1,24 @@
 namespace espacioEmpleado;
 
+
+enum cargos {
+    Auxiliar = 0,
+    Administrativo = 1,
+    Ingeniero = 2,
+    Especialista = 3,
+    Investigador = 4
+}
 public class empleado
 {
-    [Flags]
-    enum cargos {
-        Auxiliar = 0,
-        Administrativo = 1,
-        Ingeniero = 2,
-        Especialista = 3,
-        Investigador = 4
-    }
     private string? Nombre;
     private string? Apellido;
     private DateTime FNac;
     private char Civil;
     private char Genero;
     private DateTime FIngreso;
-
-    public empleado(int x, string Nombre, string Apellido, DateTime FNac, char Civil, char Genero, DateTime FIngreso)
+    
+    public empleado(string cargo, int x, string Nombre, string Apellido, DateTime FNac, char Civil, char Genero, DateTime FIngreso)
     {
-       
+        
     }
 }
